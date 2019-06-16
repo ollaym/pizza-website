6bdc27d781116b184c8ed83833a43430e2fc4e2b
Oliver <muriithioliver11@outlook.com>
2019-06-16 13:37:10 +0300

    This commit will add the carousel js code

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$('#gallery-carousel').owlCarousel({
+    autoplay:true,
+    dots:false,
+    nav: true,
+    loop:true,
+    navText: ['<i class= "fa fa-angle-left"></i>', '<i class= "fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
