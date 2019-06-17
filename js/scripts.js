3c4b3dc4fd3ec55bd20608466aa90bb10b12f3c9
Oliver <muriithioliver11@outlook.com>
2019-06-17 09:46:24 +0300

    This commit will add the js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,53 @@
 $('#gallery-carousel').owlCarousel({
-    autoplay:true,
-    dots:false,
+    autoplay: true,
+    dots: false,
     nav: true,
-    loop:true,
+    loop: true,
     navText: ['<i class= "fa fa-angle-left"></i>', '<i class= "fa fa-angle-right"></i>'],
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:2
+        1000: {
+            items: 2
         }
     }
+})
+// business logic
+function Pizza(pizza, topping, crust) {
+    this.pizza = pizza;
+    this.topping = topping;
+    this.crust = crust;
+}
+
+Concat.prototype.wholePizza = function () {
+    return this.pizza + " " + this.topping + " " + this.crust;
+}
+
+// user ntyerface logic
+$(document).ready(function () {
+    $(".button").click(function (event) {
+        event.preventDefault();
+
+        var inputPizza = $(".button").val();
+        var inputTopping = $(".button").val();
+        var inputCrust = $(".button").val();
+
+        var myPizza = new Pizza(inputPizza, inputTopping, inputCrust);
+
+        $("ul#pizzas").append("<li><span class='pizza'>" + myPizza.wholePizza() + "</span></li>");
+        $(".button").val("");
+        $(".button").val("");
+        $(".button").val("");
+        $(".pizza").last().click(function () {
+            $("#show-pizza").show();
+            $("#show-pizza h4").show();
+            $(".pizza").text(myPizza.pizza);
+            $(".topping").text(myPizza.topping);
+            $(".crust").text(myPizza.crust);
+        })
+    });
 })
