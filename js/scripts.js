926199649b1fbca79d704ffe359f72c0a6d28cef
Oliver <muriithioliver11@outlook.com>
2019-06-18 08:47:21 +0300

    This commit will debug all

--- js/scripts.js
+++ js/scripts.js
@@ -19,30 +19,31 @@ $('#gallery-carousel').owlCarousel({
 
 $(document).ready(function () {
 
-    function Pizza (name, size, crust, topping, location, orderNo) {
+    function Pizza(name, size, crust, topping, location, orderNo) {
         this.orderNo = orderNo;
         this.name = name;
         this.size = size;
         this.crust = crust;
-        // this.topping = topping;
-        // this.location = location;
+        this.topping = topping;
+        this.location = location;
     };
+    
     $("#submit").submit(function (event) {
         event.preventDefault();
         var nName = $(".pizza option:selected").val();
         var nSize = $(".size option:selected").val();
         var nCrust = $(".crust option:selected").val();
-        // var nTopping = $(".toppings option:selected").val();
-        // var nLocation = $(".delivery option:selected").val();
-        var total = parseInt(nSize) + parseInt(nCrust);
-        var order = 1;
+        var nTopping = $(".toppings option:selected").val();
+        var nLocation = $(".delivery option:selected").val();
+        var total = parseInt(nSize) + parseInt(nCrust) + parseInt(nTopping) + parseInt(nLocation);
+        var newPizza = new Pizza(nName, nSize, nCrust, nTopping, nLocation);
+        order = 0;
         order = order + 1;
-        var newPizza = new Pizza(nName, nSize, nCrust);
-        var newRow = '<tr> <th scope = "row" > ' + order + '<td id = "name" > ' + newPizza.name + '<td id = "size" > ' + newPizza.size + '<td id = "crust" > ' + newPizza.crust + '<td id = "topping" > ' + newPizza.topping + '<td id = "delivery" > ' + newPizza.delivery + '<td id = "total" > ' + total
+        var newRow = '<tr> <th scope = "row" > ' + order + '<td id = "name" > ' + newPizza.name + '<td id = "size" > ' + $(".size option:selected").text() + ":" + newPizza.size + '<td id = "crust" > ' + $(".crust option:selected").text() + ":"+ newPizza.crust + '<td id = "topping" > '+ $(".toppings option:selected").text() + ":" + newPizza.topping + '<td id = "delivery" > '+ $(".delivery option:selected").text() + ":" + newPizza.location + '<td id = "total" > ' + total
 
         $("#pizza").append(newRow);
 
-       
+
     });
 
 
